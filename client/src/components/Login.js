12555e4f6a529fd07e569d36ab9f5f164912f4b3
AjileJ <53097022+AjileJ@users.noreply.github.com>
2019-10-21 11:04:56 -0700

    login and register started

---
+++ client/src/components/Login.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+
+const Login = props => {
+  const [logini, setLogini] = useState({ username: "", password: "" });
+  const changeHandler = event => {
+    event.preventDefault();
+    setLogini({
+      ...logini,
+      [event.target.name]: event.target.value
+    });
+  };
+  const handleSubmit = event => {
+    event.preventDefault();
+    axios
+      .post("https://mypartyplanner.herokuapp.com/api/auth/login", logini)
+      .then(response => {
+        localStorage.setItem("token", response.data.token);
+        props.history.push("/tabs");
+      })
+      .catch(err => console.log("error in handlesSub", err.response));
+    setLogini({ username: "", password: "" });
+  };
+  return (
+    <div className="loginwrapper">
+      <div className="loginContainer">
+        <div className="PartyHead">
+          <h1>Tabless Thursday</h1>
+        </div>
+        <div className = 'formwrapper'>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="name"
+            placeholder="enter username"
+            type="text"
+            value={logini.username}
+            name="username"
+            onChange={changeHandler}
+          />
+          <input
+            className="password"
+            placeholder="enter password"
+            type="password"
+            value={logini.password}
+            name="password"
+            onChange={changeHandler}
+          />
+          <div className = 'buttonwrapper'>
+          <button type="submit" className="SubmitButtonn">
+            Login!
+          </button>
+          </div>
+          <span>
+            Dont have an account? <Link to="./Register">Register !</Link>
+          </span>
+        </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ client/src/components/Tabs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Tabs = () => {
+  return (
+    <div>
+      <h1>Tabs page</h1>
+    </div>
+  )
+}
+
+export default Tabs;
