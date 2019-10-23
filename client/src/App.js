1c323a71bb71341f564c3b65972a0da9a5ac7fda
AjileJ <53097022+AjileJ@users.noreply.github.com>
2019-10-23 15:10:00 -0700

    did a lot of styling still need to make some changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import Tabs from './components/Tabs';
 import Login from './components/Login';
 import Register from './components/Register';
 import {BrowserRouter as Router , Route, Switch, Link} from 'react-router-dom';
+import Nav from './components/Nav';
 import './App.css';
 
 function App() {
   return (
     <Router>
     <div className="App">
+      <Nav/>
       <Switch>
       <PrivateRoute exact path  = '/tabs' component = {Tabs}/>
       <Route exact path  = '/' component = {Login}/>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,92 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {login} from './actions/index';
+import styled from 'styled-components';
+
+
+const size = {
+  laptop: '1024px'
+}
+const device = {
+  laptop: `(min-width: ${size.laptop})`
+}
+
+const LoginTitle = styled.h1`
+  color: black;
+  font-size: 2.8rem;
+  margin: 0;
+  padding:2%;
+  padding-bottom: 7%;
+  font-family: 'Noto Serif TC', serif;
+`;
+const FormWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  
+`;
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  padding: 3px 5px 3px 5px;
+  margin: 3%;
+  color: #fffafa;
+  background: #22283a;
+  border-radius: 5px;
+  padding: 4px 8px 4px 8px;
+  font-size: 0.9rem;
+  border: 2px solid #dcdcdc;
+  margin-top:13%;
+  &:hover {
+    color: gray;
+    background: black;
+  }
+`;
+const MainWrapper = styled.div`
+
+padding-bottom: 9%;
+display:flex;
+justify-content: center;
+
+`;
+const Cta = styled.div`
+box-shadow: 10px 10px 5px gray;
+margin-top:18%;
+border: 4px solid #00000082;
+border-radius: 10px;
+padding: 70px 1px 120px 1px;
+background-color: #01010c4d;
+width:50%;
+font-family: 'Noto Serif TC', serif;
+@media ${device.laptop} {
+margin-top:12%;
+border: 4px solid #00000082;
+border-radius: 10px;
+padding: 70px 1px 120px 1px;
+background-color: #01010c4d;
+width:40%;
+font-family: 'Noto Serif TC', serif;
+}
+
+`;
+const SpanReg = styled.div`
+padding-top: 2%;
+`;
+
 
 
 const Login = props => {
+  const {id} = props
+  useEffect(() => {
+    if(id){
+      props.history.push('/tabs')
+    }
+  },[id])
   const [logini, setLogini] = useState({ username: "", password: "" });
   const changeHandler = event => {
     event.preventDefault();
@@ -17,20 +98,19 @@ const Login = props => {
   const handleSubmit = event => {
     event.preventDefault();
     props.login(logini);
-    props.history.push("/tabs");
-    setLogini({ username: "", password: "" });
+    // props.history.push("/tabs");
+    // setLogini({ username: "", password: "" });
   };
   return (
-    <div className="loginwrapper">
-      <div className="loginContainer">
-        <div className="PartyHead">
-          <h1>Tabless Thursday</h1>
-        </div>
-        <div className = 'formwrapper'>
+    <MainWrapper>
+        <Cta>
+          <LoginTitle>Tabless Thursday</LoginTitle>
+        
+        <FormWrapper>
         <form onSubmit={handleSubmit}>
           <input
             className="name"
-            placeholder="enter username"
+            placeholder="username"
             type="text"
             value={logini.username}
             name="username"
@@ -38,24 +118,24 @@ const Login = props => {
           />
           <input
             className="password"
-            placeholder="enter password"
+            placeholder="password"
             type="password"
             value={logini.password}
             name="password"
             onChange={changeHandler}
           />
-          <div className = 'buttonwrapper'>
-          <button type="submit" className="SubmitButtonn">
+          <ButtonWrapper>
+          <Button type="submit" >
             Login!
-          </button>
-          </div>
-          <span>
+          </Button>
+          </ButtonWrapper>
+          <SpanReg>
             Dont have an account? <Link to="./Register">Register !</Link>
-          </span>
+          </SpanReg>  
         </form>
-        </div>
-      </div>
-    </div>
+        </FormWrapper>
+        </Cta>
+    </MainWrapper>
   );
 };
 
